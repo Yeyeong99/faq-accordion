fd91aeb8f6b347b86da95eeb83f9135353c32c50
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2020-10-28 22:50:36 +0900

    css 수정 및 js 동작 확인

--- faq-accordion-card-main/index.js
+++ faq-accordion-card-main/index.js
@@ -1,10 +1,22 @@
-import "./styles.css";
 
-const askBox = document.querySelector(".ask")
-const title = askBox.querySelector(".title")
-function changeColor() {
-    title.classList.remove("title")
-    title.classList.add("hover")
+const askBox = document.querySelector(".ask");
+const title = askBox.querySelector(".title");
+const explain = askBox.querySelector(".explain");
+
+const handleTitle = {
+    hover: function() {
+        title.classList.add("hover");
+    },
+    leave: function() {
+        title.classList.remove("hover");
+    },
+    enter: function() {
+        title.classList.remove("hover");
+        explain.classList.remove("explain")
+        explain.classList.add("show");
+    }
 }
 
-title.addEventListener("mouseenter",changeColor)
+title.addEventListener("mouseenter",handleTitle.hover);
+title.addEventListener("mouseleave",handleTitle.leave);
+title.addEventListener("click", handleTitle.enter);
