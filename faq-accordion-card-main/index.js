9d9d5eddf90d249a7d52e9660cba8af9e82bbb43
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2020-10-30 00:45:43 +0900

    움직임 추가

--- faq-accordion-card-main/index.js
+++ faq-accordion-card-main/index.js
@@ -13,5 +13,7 @@ for (i=0; i<title.length; i++) {
         this.classList.toggle("show");
         this.nextElementSibling.classList.toggle("explain");
         this.nextElementSibling.classList.toggle("show");
+        title.not(this).nextElementSibling.classList.remove("show");
+        title.not(this).classList.remove("show");
     })
 };
