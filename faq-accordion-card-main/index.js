60a1df1c169b06caab7ab33ef29ae3f85e764f16
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2020-10-29 21:08:03 +0900

    자바스크립트 수정

--- faq-accordion-card-main/index.js
+++ faq-accordion-card-main/index.js
@@ -1,22 +1,36 @@
 
-const askBox = document.querySelector(".ask");
-const title = askBox.querySelector(".title");
-const explain = askBox.querySelector(".explain");
+const askBox = document.querySelectorAll(".ask");
+const title = askBox.querySelectorAll(".title");
+const explain = askBox.querySelectorAll(".explain");
+const button = askBox.querySelectorAll(".button")
 
-const handleTitle = {
-    hover: function() {
-        title.classList.add("hover");
-    },
-    leave: function() {
-        title.classList.remove("hover");
-    },
-    enter: function() {
-        title.classList.remove("hover");
-        explain.classList.remove("explain")
-        explain.classList.add("show");
-    }
+title.forEach(function(eachBtn) {
+    eachBtn.addEventListener("click",handleTitle)
+    
+});
+
+function handleTitle(eachBtn) {
+    const 
 }
+// const handleTitle = {
+//     hover: function() {
+//         title.classList.add("hover");
+//     },
+//     leave: function() {
+//         title.classList.remove("hover");
+//     },
+//     enter: function() {
+//         title.classList.remove("hover");
+//         explain.classList.remove("explain")
+//         explain.classList.add("show");
+//     },
+//     back : function() {
+//         explain.classList.remove("show")
+//         explain.classList.add("explain")
+//     }
+// }
 
-title.addEventListener("mouseenter",handleTitle.hover);
-title.addEventListener("mouseleave",handleTitle.leave);
-title.addEventListener("click", handleTitle.enter);
+// title.addEventListener("mouseenter",handleTitle.hover);
+// title.addEventListener("mouseleave",handleTitle.leave);
+// title.addEventListener("click", handleTitle.enter);
+// button.addEventListener("click",handleTitle.back);
