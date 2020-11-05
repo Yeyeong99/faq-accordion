f068e1377734a2fb2c3c437540b9fa6be2dda7e8
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2020-11-05 23:52:56 +0900

    js 모션 수정

--- faq-accordion-card-main/index.js
+++ faq-accordion-card-main/index.js
@@ -2,6 +2,17 @@ const title = document.querySelectorAll(".title");
 const explain = document.querySelectorAll(".explain");
 const button = document.querySelectorAll(".button");
 
+
+function handleClick() {
+    const hasClass = title.classList.contains(show);
+  
+    if (!hasClass) {
+      title.classList.add(show);
+    } else {
+      title.classList.remove(show);
+    }
+  }
+
 for (i=0; i<=title.length; i++) {
     title[i].addEventListener("mouseenter",function(){
         this.classList.add("hover");
@@ -10,13 +21,17 @@ for (i=0; i<=title.length; i++) {
         this.classList.remove("hover");
     }),
     title[i].addEventListener("click", function(event){ //event 가 클릭했을 때 target을 알 수 있게 해줌
-        for (j=0; j<=explain.length; j++){
-            this.classList.add("show");
-            this.nextElementSibling.classList.add("show");
-            explain[j].previousElementSibling.classList.remove("show");
-            explain[j].classList.remove("show");
+        if (this.classList.contains("show")) {
+            this.classList.remove("show");
+            this.nextElementSibling.classList.remove("show");
+        }else{
+            for (j=0; j<=explain.length; j++){
+                console.log(this)
+                this.classList.add("show");
+                this.nextElementSibling.classList.add("show");
+                explain[j].previousElementSibling.classList.remove("show");
+                explain[j].classList.remove("show");
         }
+    }
     })
 };
-
-
